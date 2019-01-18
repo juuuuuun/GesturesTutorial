18ef57b50f69416c6fe14953f1ad4fc4c5039c17
Jun Oh <kyungjunjoh@gmail.com>
2019-01-17 17:13:17 -0800

    Initial Commit

---
+++ GesturesTutorial/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GesturesTutorial
+//
+//  Created by Jun Oh on 2019-01-17.
+//  Copyright © 2019 Jun Oh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GesturesTutorialTests/GesturesTutorialTests.m
@@ -0,0 +1,37 @@
+//
+//  GesturesTutorialTests.m
+//  GesturesTutorialTests
+//
+//  Created by Jun Oh on 2019-01-17.
+//  Copyright © 2019 Jun Oh. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface GesturesTutorialTests : XCTestCase
+
+@end
+
+@implementation GesturesTutorialTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
